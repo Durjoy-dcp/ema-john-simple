e4377baaa8f9336a51465fe813bb060971684ff8
Durjoy-dcp <unknownccxv@gmail.com>
2022-09-27 12:47:47 +0600

    header done

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import logo from '../../images/Logo.svg';
+import './Header.css';
 
 const Header = () => {
     return (
-        <div>
-            <h1>This is header</h1>
+        <div className='header'>
+            <nav>
+                <div>
+
+                    <img src={logo} alt="" />
+                </div>
+                <div><a href="">Order</a>
+                    <a href="">Order Review</a>
+                    <a href="">Manage Inventory</a>
+                    <a href="">Login</a>
+
+                </div>
+
+            </nav>
+
         </div>
     );
 };
